87e224a035139e67f0cd6040935bbda00fed0265
Paul Voelker <healingwaters7@hotmail.com>
2023-12-07 13:47:55 -0600

    Simplify code in reaction time

--- BuzzBoxGames.ViewModel/Game/ReactionTime.cs
+++ BuzzBoxGames.ViewModel/Game/ReactionTime.cs
@@ -30,36 +30,36 @@ namespace BuzzBoxGames.ViewModel.Game
         {
             GameState = GameStateEnum.Done;
 
-            PaddleRed1Time = (double?)e.Red1Time;
-            PaddleRed2Time = (double?)e.Red2Time;
-            PaddleRed3Time = (double?)e.Red3Time;
-            PaddleRed4Time = (double?)e.Red4Time;
+            Red1Paddle.Time = (double?)e.Red1Time;
+            Red2Paddle.Time = (double?)e.Red2Time;
+            Red3Paddle.Time = (double?)e.Red3Time;
+            Red4Paddle.Time = (double?)e.Red4Time;
 
-            PaddleGreen1Time = (double?)e.Green1Time;
-            PaddleGreen2Time = (double?)e.Green2Time;
-            PaddleGreen3Time = (double?)e.Green3Time;
-            PaddleGreen4Time = (double?)e.Green4Time;
+            Green1Paddle.Time = (double?)e.Green1Time;
+            Green2Paddle.Time = (double?)e.Green2Time;
+            Green3Paddle.Time = (double?)e.Green3Time;
+            Green4Paddle.Time = (double?)e.Green4Time;
 
-            IsPaddleRed1Winner = false;
-            IsPaddleRed2Winner = false;
-            IsPaddleRed3Winner = false;
-            IsPaddleRed4Winner = false;
+            Red1Paddle.IsWinner = false;
+            Red2Paddle.IsWinner = false;
+            Red3Paddle.IsWinner = false;
+            Red4Paddle.IsWinner = false;
 
-            IsPaddleGreen1Winner = false;
-            IsPaddleGreen2Winner = false;
-            IsPaddleGreen3Winner = false;
-            IsPaddleGreen4Winner = false;
+            Green1Paddle.IsWinner = false;
+            Green2Paddle.IsWinner = false;
+            Green3Paddle.IsWinner = false;
+            Green4Paddle.IsWinner = false;
 
             var timesList = new List<Tuple<Action, double?>>
             {
-                new(() => { IsPaddleRed1Winner = true; }, PaddleRed1Time),
-                new(() => { IsPaddleRed2Winner = true; }, PaddleRed2Time),
-                new(() => { IsPaddleRed3Winner = true; }, PaddleRed3Time),
-                new(() => { IsPaddleRed4Winner = true; }, PaddleRed4Time),
-                new(() => { IsPaddleGreen1Winner = true; }, PaddleGreen1Time),
-                new(() => { IsPaddleGreen2Winner = true; }, PaddleGreen2Time),
-                new(() => { IsPaddleGreen3Winner = true; }, PaddleGreen3Time),
-                new(() => { IsPaddleGreen4Winner = true; }, PaddleGreen4Time)
+                new(() => { Red1Paddle.IsWinner = true; }, Red1Paddle.Time),
+                new(() => { Red2Paddle.IsWinner = true; }, Red2Paddle.Time),
+                new(() => { Red3Paddle.IsWinner = true; }, Red3Paddle.Time),
+                new(() => { Red4Paddle.IsWinner = true; }, Red4Paddle.Time),
+                new(() => { Green1Paddle.IsWinner = true; }, Green1Paddle.Time),
+                new(() => { Green2Paddle.IsWinner = true; }, Green2Paddle.Time),
+                new(() => { Green3Paddle.IsWinner = true; }, Green3Paddle.Time),
+                new(() => { Green4Paddle.IsWinner = true; }, Green4Paddle.Time)
             };
 
             var minTime = timesList.MaxBy(x => x.Item2)?.Item2;
@@ -104,182 +104,29 @@ namespace BuzzBoxGames.ViewModel.Game
         public bool GameIsStarted { get => _gameState == GameStateEnum.Started; }
         public bool GameIsDone { get => _gameState == GameStateEnum.Done; }
 
-        public double MaxTime { get => 1000; }
+        private readonly ReactionTimePaddle _red1Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Red1Paddle { get => _red1Paddle; }
 
-        private double? _paddleRed1Time = 0;
-        public double? PaddleRed1Time
-        {
-            get => _paddleRed1Time;
-            private set
-            {
-                SetProperty(ref _paddleRed1Time, value);
-                OnPropertyChanged(nameof(PaddleRed1Score));
-                OnPropertyChanged(nameof(DidPaddleRed1BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleRed1NotBuzzIn));
-            }
-        }
-        public double? PaddleRed1Score { get => MaxTime - _paddleRed1Time; }
-        public bool DidPaddleRed1BuzzIn { get => _paddleRed1Time != null; }
-        public bool DidPaddleRed1NotBuzzIn { get => _paddleRed1Time == null; }
-        private bool _isPaddleRed1Winner = false;
-        public bool IsPaddleRed1Winner
-        {
-            get => _isPaddleRed1Winner;
-            private set => SetProperty(ref _isPaddleRed1Winner, value);
-        }
+        private readonly ReactionTimePaddle _red2Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Red2Paddle { get => _red2Paddle; }
 
-        private double? _paddleRed2Time = 0;
-        public double? PaddleRed2Time
-        {
-            get => _paddleRed2Time;
-            private set
-            {
-                SetProperty(ref _paddleRed2Time, value);
-                OnPropertyChanged(nameof(PaddleRed2Score));
-                OnPropertyChanged(nameof(DidPaddleRed2BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleRed2NotBuzzIn));
-            }
-        }
-        public double? PaddleRed2Score { get => MaxTime - _paddleRed2Time; }
-        public bool DidPaddleRed2BuzzIn { get => _paddleRed2Time != null; }
-        public bool DidPaddleRed2NotBuzzIn { get => _paddleRed2Time == null; }
-        private bool _isPaddleRed2Winner = false;
-        public bool IsPaddleRed2Winner
-        {
-            get => _isPaddleRed2Winner;
-            private set => SetProperty(ref _isPaddleRed2Winner, value);
-        }
+        private readonly ReactionTimePaddle _red3Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Red3Paddle { get => _red3Paddle; }
 
-        private double? _paddleRed3Time = 0;
-        public double? PaddleRed3Time
-        {
-            get => _paddleRed3Time;
-            private set
-            {
-                SetProperty(ref _paddleRed3Time, value);
-                OnPropertyChanged(nameof(PaddleRed3Score));
-                OnPropertyChanged(nameof(DidPaddleRed3BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleRed3NotBuzzIn));
-            }
-        }
-        public double? PaddleRed3Score { get => MaxTime - _paddleRed3Time; }
-        public bool DidPaddleRed3BuzzIn { get => _paddleRed3Time != null; }
-        public bool DidPaddleRed3NotBuzzIn { get => _paddleRed3Time == null; }
-        private bool _isPaddleRed3Winner = false;
-        public bool IsPaddleRed3Winner
-        {
-            get => _isPaddleRed3Winner;
-            private set => SetProperty(ref _isPaddleRed3Winner, value);
-        }
+        private readonly ReactionTimePaddle _red4Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Red4Paddle { get => _red4Paddle; }
 
-        private double? _paddleRed4Time = 0;
-        public double? PaddleRed4Time
-        {
-            get => _paddleRed4Time;
-            private set
-            {
-                SetProperty(ref _paddleRed4Time, value);
-                OnPropertyChanged(nameof(PaddleRed4Score));
-                OnPropertyChanged(nameof(DidPaddleRed4BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleRed4NotBuzzIn));
-            }
-        }
-        public double? PaddleRed4Score { get => MaxTime - _paddleRed4Time; }
-        public bool DidPaddleRed4BuzzIn { get => _paddleRed4Time != null; }
-        public bool DidPaddleRed4NotBuzzIn { get => _paddleRed4Time == null; }
-        private bool _isPaddleRed4Winner = false;
-        public bool IsPaddleRed4Winner
-        {
-            get => _isPaddleRed4Winner;
-            private set => SetProperty(ref _isPaddleRed4Winner, value);
-        }
 
-        private double? _paddleGreen1Time = 0;
-        public double? PaddleGreen1Time
-        {
-            get => _paddleGreen1Time;
-            private set
-            {
-                SetProperty(ref _paddleGreen1Time, value);
-                OnPropertyChanged(nameof(PaddleGreen1Score));
-                OnPropertyChanged(nameof(DidPaddleGreen1BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleGreen1NotBuzzIn));
-            }
-        }
-        public double? PaddleGreen1Score { get => MaxTime - _paddleGreen1Time; }
-        public bool DidPaddleGreen1BuzzIn { get => _paddleGreen1Time != null; }
-        public bool DidPaddleGreen1NotBuzzIn { get => _paddleGreen1Time == null; }
-        private bool _isPaddleGreen1Winner = false;
-        public bool IsPaddleGreen1Winner
-        {
-            get => _isPaddleGreen1Winner;
-            private set => SetProperty(ref _isPaddleGreen1Winner, value);
-        }
+        private readonly ReactionTimePaddle _green1Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Green1Paddle { get => _green1Paddle; }
 
-        private double? _paddleGreen2Time = 0;
-        public double? PaddleGreen2Time
-        {
-            get => _paddleGreen2Time;
-            private set
-            {
-                SetProperty(ref _paddleGreen2Time, value);
-                OnPropertyChanged(nameof(PaddleGreen2Score));
-                OnPropertyChanged(nameof(DidPaddleGreen2BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleGreen2NotBuzzIn));
-            }
-        }
-        public double? PaddleGreen2Score { get => MaxTime - _paddleGreen2Time; }
-        public bool DidPaddleGreen2BuzzIn { get => _paddleGreen2Time != null; }
-        public bool DidPaddleGreen2NotBuzzIn { get => _paddleGreen2Time == null; }
-        private bool _isPaddleGreen2Winner = false;
-        public bool IsPaddleGreen2Winner
-        {
-            get => _isPaddleGreen2Winner;
-            private set => SetProperty(ref _isPaddleGreen2Winner, value);
-        }
+        private readonly ReactionTimePaddle _green2Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Green2Paddle { get => _green2Paddle; }
 
-        private double? _paddleGreen3Time = 0;
-        public double? PaddleGreen3Time
-        {
-            get => _paddleGreen3Time;
-            private set
-            {
-                SetProperty(ref _paddleGreen3Time, value);
-                OnPropertyChanged(nameof(PaddleGreen3Score));
-                OnPropertyChanged(nameof(DidPaddleGreen3BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleGreen3NotBuzzIn));
-            }
-        }
-        public double? PaddleGreen3Score { get => MaxTime - _paddleGreen2Time; }
-        public bool DidPaddleGreen3BuzzIn { get => _paddleGreen3Time != null; }
-        public bool DidPaddleGreen3NotBuzzIn { get => _paddleGreen3Time == null; }
-        private bool _isPaddleGreen3Winner = false;
-        public bool IsPaddleGreen3Winner
-        {
-            get => _isPaddleGreen3Winner;
-            private set => SetProperty(ref _isPaddleGreen3Winner, value);
-        }
+        private readonly ReactionTimePaddle _green3Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Green3Paddle { get => _green3Paddle; }
 
-        private double? _paddleGreen4Time = 0;
-        public double? PaddleGreen4Time
-        {
-            get => _paddleGreen4Time;
-            private set
-            {
-                SetProperty(ref _paddleGreen4Time, value);
-                OnPropertyChanged(nameof(PaddleGreen4Score));
-                OnPropertyChanged(nameof(DidPaddleGreen4BuzzIn));
-                OnPropertyChanged(nameof(DidPaddleGreen4NotBuzzIn));
-            }
-        }
-        public double? PaddleGreen4Score { get => MaxTime - _paddleGreen2Time; }
-        public bool DidPaddleGreen4BuzzIn { get => _paddleGreen4Time != null; }
-        public bool DidPaddleGreen4NotBuzzIn { get => _paddleGreen4Time == null; }
-        private bool _isPaddleGreen4Winner = false;
-        public bool IsPaddleGreen4Winner
-        {
-            get => _isPaddleGreen4Winner;
-            private set => SetProperty(ref _isPaddleGreen4Winner, value);
-        }
+        private readonly ReactionTimePaddle _green4Paddle = new ReactionTimePaddle();
+        public ReactionTimePaddle Green4Paddle { get => _green4Paddle; }
     }
 }
---
+++ BuzzBoxGames.ViewModel/Game/ReactionTimePaddle.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzBoxGames.ViewModel.Game
+{
+    public class ReactionTimePaddle : ObservableObject
+    {
+        public double MaxTime { get => 1000; }
+
+        private double? _time = 0;
+        public double? Time
+        {
+            get => _time;
+            set
+            {
+                SetProperty(ref _time, value);
+                OnPropertyChanged(nameof(Score));
+                OnPropertyChanged(nameof(BuzzedIn));
+                OnPropertyChanged(nameof(NotBuzzedIn));
+            }
+        }
+        public double? Score { get => MaxTime - _time; }
+        public bool BuzzedIn { get => _time != null; }
+        public bool NotBuzzedIn { get => _time == null; }
+
+        private bool _isWinner = false;
+        public bool IsWinner
+        {
+            get => _isWinner;
+            set => SetProperty(ref _isWinner, value);
+        }
+    }
+}
